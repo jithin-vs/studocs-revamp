f12c28cb9953140dc472deae030a453d2c3e6d63
jithin <jithinvs1045@gmail.com>
2025-01-12 23:03:30 +0530

    header content and basic placing done

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Image from 'next/image'
+import logo from '../../../public/assets/img/logo.png'
 
 export default function Header() {
   return (
-    <div className='bg-primary'>
-       <nav>
-        <p>home</p>
+    <div className='bg-primary flex justify-between'>
+      <div className='flex'>
+       <Image 
+       src={logo}
+       alt='logo'
+       width={32}
+       height={32}
+       />
+       <h1>STUDOCS</h1>
+       </div>
+       <nav >
+        <div className='flex justify-between gap-5 px-4'>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Team</a>
+        <a href="#">Contact</a>
+        <a href="#" className='rounded-md'>Get Started</a>
+        </div>
        </nav>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
   module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
       extend: {
         colors:{
           primary:"#37517e",
+          secondary:"#73c5eb"
         }
       },
     },
