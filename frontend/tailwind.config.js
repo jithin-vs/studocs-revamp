4fac919eb80a9de1ef3572acbc00d0fa4509feec
jithin <jithinvs1045@gmail.com>
2025-01-16 00:13:41 +0530

    added open-sans font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,12 @@
         colors:{
           primary:"#37517e",
           secondary:"#73c5eb"
-        }
+        },
+        fontFamily: {
+          // If using variable
+          sans: ['var(--font-opensans)'],
+          opensans: ['Open Sans', 'sans-serif'],
+        },
       },
     },
     plugins: [],
