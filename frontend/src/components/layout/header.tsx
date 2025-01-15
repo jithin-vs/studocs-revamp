6f2112726177866b541d1505e2ab674a8173d3b7
jithin <jithinvs1045@gmail.com>
2025-01-16 00:16:33 +0530

    header styling.responsiveness pending

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '../../../public/assets/img/logo.png'
+import React from "react";
+import Image from "next/image";
+import logo from "../../../public/assets/img/logo.png";
 
 export default function Header() {
   return (
-    <div className='bg-primary flex justify-between'>
-      <div className='flex'>
-       <Image 
-       src={logo}
-       alt='logo'
-       width={32}
-       height={32}
-       />
-       <h1>STUDOCS</h1>
-       </div>
-       <nav >
-        <div className='flex justify-between gap-5 px-4'>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Team</a>
-        <a href="#">Contact</a>
-        <a href="#" className='rounded-md'>Get Started</a>
+    <div className="bg-primary flex justify-between p-4">
+      <div className="flex">
+        <div className="flex justify-between gap-4">
+          <Image src={logo} alt="logo" width={40} height={40} />
+          <h1 className="text-4xl font-medium">STUDOCS</h1>
         </div>
-       </nav>
+      </div>
+      <nav className="flex">
+        <div  className="flex flex-wrap justify-between px-4 gap-8 items-center content-center">
+          <a className="hover:text-secondary" href="#">Home</a>
+          <a className="hover:text-secondary" href="#">About</a>
+          <a className="hover:text-secondary" href="#">Services</a>
+          <a className="hover:text-secondary" href="#">Team</a>
+          <a className="hover:text-secondary" href="#">Contact</a>
+          <a href="#" className="border border-secondary rounded-full px-5 py-2 hover:bg-secondary">
+            Get Started
+          </a>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
